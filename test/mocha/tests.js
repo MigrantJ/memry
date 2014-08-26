538ed5baa84ba5b9c93c8e6cea5485f479d4efa6
Jim Grant <jimgrant@gmail.com>
2014-08-26 09:21:55 -0700

    template files

---
+++ test/mocha/tests.js
@@ -0,0 +1,9 @@
+// tests.js
+var chai = require('chai'),
+    expect = chai.expect;
+
+describe('BasicTest', function(){
+  it('should pass a simple test', function() {
+    expect(true).to.equal(true);
+  });
+});
